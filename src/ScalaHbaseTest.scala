93666c6371bbdf50eb14f9fe6faf72ca17f41a0c
Arun Dhingra <arun.dh93@gmail.com>
2018-08-23 13:55:40 +0530

    creating new table (uses deprecated code)

--- src/ScalaHbaseTest.scala
+++ src/ScalaHbaseTest.scala
@@ -1,7 +1,7 @@
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Put}
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
 
 object ScalaHbaseTest {
 
@@ -13,10 +13,20 @@ object ScalaHbaseTest {
     //    conf.set("hbase.zookeeper.quorum", "localhost")
     //    conf.set("hbase.zookeeper.property.clientPort", "2181")
     val connection: Connection = ConnectionFactory.createConnection(conf)
+
+    // add to existing table
     val table = connection.getTable(TableName.valueOf("emp"))
     val put1: Put = new Put(Bytes.toBytes("row5"))
     put1.addColumn(Bytes.toBytes("personal data"), Bytes.toBytes("qual5"), Bytes.toBytes("val5"))
     table.put(put1)
+
+    //create new table
+    val admin = connection.getAdmin
+    val tableDesc = new HTableDescriptor(Bytes.toBytes("mytable"))
+    val idsColumnFamilyDesc = new HColumnDescriptor(Bytes.toBytes("ids"))
+    tableDesc.addFamily(idsColumnFamilyDesc)
+    admin.createTable(tableDesc)
+
     println("Success")
   }
 }
